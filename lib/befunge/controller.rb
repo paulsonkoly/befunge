9e29a8016d344acf6a16fefd55cef7767caf174a
Paul Sonkoly <sonkoly.pal@gmail.com>
2017-08-26 10:24:08 +0100

    100% module level coverage for Controller

--- lib/befunge/controller.rb
+++ lib/befunge/controller.rb
@@ -6,6 +6,10 @@ module Befunge
       Vector.new(x + other.x, y + other.y)
     end
 
+    def -(other)
+      Vector.new(x - other.x, y - other.y)
+    end
+
     def *(number)
       Vector.new(number * x, number * y)
     end
--- spec/module/controller_spec.rb
+++ spec/module/controller_spec.rb
@@ -46,6 +46,59 @@ describe Befunge::Controller do
         expect { subject.move! }.to change(subject, :position)
       end
     end
+
+    context 'after calling #_(0)' do
+      before :each { subject._(0) }
+
+      it 'moves to the right' do
+        expect { subject.move! }.to change(subject, :position)
+          .by(Befunge::Vector.new(1, 0))
+      end
+    end
+
+    context 'after calling #_(1)' do
+      before :each { subject._(1) }
+
+      it 'moves to the left' do
+        expect { subject.move! }.to change(subject, :position)
+          .by(Befunge::Vector.new(-1, 0))
+      end
+    end
+
+    context 'after calling #|(0)' do
+      before :each { subject.|(0) }
+
+      it 'moves down' do
+        expect { subject.move! }.to change(subject, :position)
+          .by(Befunge::Vector.new(0, 1))
+      end
+    end
+
+    context 'after calling #|(1)' do
+      before :each { subject.|(1) }
+
+      it 'moves up' do
+        expect { subject.move! }.to change(subject, :position)
+          .by(Befunge::Vector.new(0, -1))
+      end
+    end
+
+    context 'after #trampoline' do
+      before :each { subject.trampoline! }
+
+      it 'jumps over the next cell' do
+        expect { subject.move! }.to change(subject, :position)
+          .by(Befunge::Vector.new(2, 0))
+      end
+
+      context 'after a move has been made' do
+        before :each { subject.move! }
+
+        it 'is reset to normal movement' do
+          expect { subject.move! }.to change(subject, :position)
+            .by(Befunge::Vector.new(1, 0))
+        end
+      end
+    end
   end
 end
-
