d5f12d9f4670f72f70d902882a0b635bbd12066c
Paul Sonkoly <sonkoly.pal@gmail.com>
2017-08-26 14:12:30 +0100

    Full module level specs for interpreter

--- lib/befunge/interpreter.rb
+++ lib/befunge/interpreter.rb
@@ -3,37 +3,34 @@ require_relative 'controller'
 
 module Befunge
   class Interpreter
-    attr_reader :output
-
     def initialize(alu = Befunge::ALU.new,
                    controller = Befunge::Controller.new,
                    memory = Befunge::Memory.new)
-      @output = ''
       @string_mode = false
       @alu = alu
       @controller = controller
       @memory = memory
     end
 
+    def output
+      @memory.output
+    end
+
     def run(program)
       @memory.program = program.split("\n")
-      step while operator != '@'
+      step while @memory[@controller.position] != '@'
     end
 
     private
 
-    def operator
-      @operator ||= @memory[@controller.position]
-    end
-
     def step
       dispatch
       @controller.move!
-      @operator = nil
       self
     end
 
     def dispatch
+      operator = @memory[@controller.position]
       if @string_mode
         @alu.push(operator.ord)
       else
@@ -46,18 +43,15 @@ module Befunge
         when /[<>^v]/ then @controller.direction = operator.to_sym
         when '?'      then @controller._?
         when /[_|]/   then @controller.send(operator, @alu.pop)
-        when '.'      then write(@alu.pop.to_s)
-        when ','      then write(@alu.pop.chr)
+        when '.'      then @memory.write(@alu.pop.to_s)
+        when ','      then @memory.write(@alu.pop.chr)
         when '"'      then string_mode!
         when ' '      then
         when 'p'      then put!
+        when 'g'      then get!
         else send(operator)
         end
       end
-    end
-
-    def write(elem)
-      @output << elem
     end
 
     def string_mode!
@@ -68,13 +62,13 @@ module Befunge
       y = @alu.pop
       x = @alu.pop
       v = @alu.pop
-      @program[x][y] = v.chr
+      @memory[Vector.new(x, y)] = v.chr
     end
 
     def get!
       y = @alu.pop
       x = @alu.pop
-      @alu.push(@program[x][y].ord)
+      @alu.push(@memory[Vector.new(x, y)].ord)
     end
   end
 end
--- spec/module/interpreter_spec.rb
+++ spec/module/interpreter_spec.rb
@@ -1,34 +1,34 @@
 require_relative '../spec_helper'
 require_relative '../../lib/befunge/interpreter.rb'
 
-def move_two_cells(controller)
-  allow(controller).to receive(:position).and_return(Befunge::Vector.new(0, 0),
-                                                     Befunge::Vector.new(1, 0))
-  allow(controller).to receive(:move!)
+def expect_cell(controller, memory, *cell)
+  expect(controller).to receive(:position).at_least(:once) # .exactly(cell.count).times
+  expect(memory).to receive(:[]).and_return(*cell)
 end
 
 describe Befunge::Interpreter do
   let :alu { double('alu') }
   let :controller { double('controller') }
-  subject { Befunge::Interpreter.new(alu, controller) }
+  let :memory { double('memory') }
+  subject { Befunge::Interpreter.new(alu, controller, memory) }
 
   (0..9).each do |i|
-    describe "#run(\"#{i}@\")" do
+    describe "opcode #{i}" do
       it "sends _#{i} to the alu" do
-        move_two_cells(controller)
+        expect_cell(controller, memory, i.to_s)
         expect(alu).to receive(:"_#{i}")
-        subject.run("#{i}@")
+        subject.send(:dispatch)
       end
     end
   end
 
   {'$' => :pop, '\\' => :swap,
    ':' => :dupl, '`' => :compare}.each do |op, message|
-    describe "#run(\"#{op}@\")" do
+    describe "opcode #{op}" do
       it "sends #{message} to the alu" do
-        move_two_cells(controller)
+        expect_cell(controller, memory, op)
         expect(alu).to receive(message)
-        subject.run("#{op}@")
+        subject.send(:dispatch)
       end
     end
   end
@@ -36,53 +36,71 @@ describe Befunge::Interpreter do
  {'<' => :direction=, '>' => :direction=,
   '^' => :direction=, 'v' => :direction=,
   '?' => :_? }.each do |op, message|
-    describe "#run(\"#{op}@\")" do
+    describe "opcode #{op}" do
       it "sends #{message} to the controller" do
-        move_two_cells(controller)
+        expect_cell(controller, memory, op)
         expect(controller).to receive(message)
-        subject.run("#{op}@")
+        subject.send(:dispatch)
       end
     end
   end
 
   ['_', '|'].each do |op|
-    describe "#run(\"#{op}@\")" do
+    describe "opcode #{op}" do
       it "sends pop to the alu, and forwards the popped value to the controller" do
-        move_two_cells(controller)
+        expect_cell(controller, memory, op)
         expect(alu).to receive(:pop).and_return(:dummy)
         expect(controller).to receive(op).with(:dummy)
-        subject.run("#{op}@")
+        subject.send(:dispatch)
       end
     end
   end
 
-  describe "#run(\".@\")" do |op|
+  describe "opcode ." do
     it "sends pop to the alu and writes that value" do
-      move_two_cells(controller)
+      expect_cell(controller, memory, '.')
       expect(alu).to receive(:pop).and_return(3)
-      subject.run(".@")
-      expect(subject.output).to eq "3"
+      expect(memory).to receive(:write).with("3")
+      subject.send(:dispatch)
     end
   end
 
-  describe "#run(\",@\")" do |op|
+  describe "opcode ," do
     it "sends pop to the alu and writes the ASCII of that value" do
-      move_two_cells(controller)
+      expect_cell(controller, memory, ',')
       expect(alu).to receive(:pop).and_return('a'.ord)
-      subject.run(",@")
-      expect(subject.output).to eq 'a'
+      expect(memory).to receive(:write).with('a')
+      subject.send(:dispatch)
     end
   end
 
-  describe "#run(\"\\\"a@\")" do
+  describe "opcode \"a" do
     it "sets itself to string mode and pushes 'a'" do
-      allow(controller).to receive(:position).and_return(
-        Befunge::Vector.new(0, 0),
-        Befunge::Vector.new(1, 0),
-        Befunge::Vector.new(2, 0))
-      allow(controller).to receive(:move!).twice
+      expect_cell(controller, memory, '"', 'a')
       expect(alu).to receive(:push).with('a'.ord)
-      subject.run("\"a@")
+      2.times { subject.send(:dispatch) }
+    end
+  end
+
+  describe 'opcode p' do
+    it "sends 3 pops to the ALU and writes to the program" do
+      expect_cell(controller, memory, 'p')
+      expect(alu).to receive(:pop).exactly(3).times.and_return(10, 20, 30)
+      expect(memory).to receive(:[]=).with(Befunge::Vector.new(20, 10), 30.chr)
+      subject.send(:dispatch)
+    end
+  end
+
+  describe 'opcode g' do
+    it "sends 2 pops to the ALU and pushes the value from the program" do
+      expect(controller).to receive(:position).at_least(:once).and_return(Befunge::Vector.new(0, 0))
+      expect(memory).to receive(:[])
+        .ordered.with(Befunge::Vector.new(0, 0)).and_return('g')
+      expect(memory).to receive(:[])
+        .ordered.with(Befunge::Vector.new(20, 10)).and_return('x')
+      expect(alu).to receive(:pop).exactly(2).times.and_return(10, 20)
+      expect(alu).to receive(:push).with('x'.ord)
+      subject.send(:dispatch)
     end
   end
 end
