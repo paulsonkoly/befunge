aed035039c3b53449177d416652db87473791a2e
Paul Sonkoly <sonkoly.pal@gmail.com>
2017-08-26 15:29:26 +0100

    Added hello world integration test, that found the previous two bugs

--- spec/integration/integration_spec.rb
+++ spec/integration/integration_spec.rb
@@ -4,8 +4,13 @@ require_relative '../../befunge'
 describe "integration" do
   subject { Befunge::Interpreter.new }
 
-  it 'return the correct result' do
+  it 'runs sesame street' do
     subject.run(">987v>.v\nv456<  :\n>321 ^ _@")
     expect(subject.output).to eq '123456789'
+  end
+
+  it 'runs the hello world' do
+    subject.run(">25*\"!dlroW olleH\":v\n               v:,_@\n                >  ^")
+    expect(subject.output).to be_empty
   end
 end
